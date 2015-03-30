fee6af44d658778876d8c0bf80ec43e735bfb7f5
Anita Amini <baadge29@yahoo.co.uk>
2015-03-30 15:34:18 +0100

    some initial tests written

--- test/__server.js
+++ test/__server.js
@@ -6,43 +6,58 @@ var server  = require('../api/server.js');
 
 //homepage '/'
 lab.experiment("When the user visits the homepage: ", function(){
-    
+
     lab.test("It should GET designs gallery", function(done){
-        
+
         var options = {
             url: '/',
             method: 'GET'
         };
-        
+
         server.inject(options, function(response){
-            
+
             assert.equal(response.statusCode, 200, "should return a status code of 200");
             assert.include(response.headers["content-type"], "image/png" || "image/jpg", "the images should load");
 			done();
         });
-        
+
     });
-	
+
 });
 
-//logging in 
+//logging in
 lab.experiment("When the user tries to login: ", function(){
-	
+
 	lab.test("if not authenticated,", function(done){
-		
+
 		var options = {
 			method: 'GET',
 			url: '/login'
 		};
-		
+
 		server.inject(options, function(response){
-			
-			assert.equal(response.statusCode, 302, "should return a FOUND status code of 302");
+
+			assert.equal(response.statusCode, 302, "should return a Found status code of 302");
+			done();
+		});
+	});
+
+	lab.test("if authenticated ", function(done){
+		var options = {
+			url: "/login",
+			method: "GET",
+			credentials : {
+				//once we decide which social site to use, look at the object and use here
+			}
+		};
+		server.inject(options, function(response){
+			assert.equal(response.statusCode, 302, "should return a Found status code of 302");
+			assert.ok(response.headers["set-cookie"], "session should be set");
 			done();
 		})
-	})
-	
-})
+	});
+
+});
 
 
 
