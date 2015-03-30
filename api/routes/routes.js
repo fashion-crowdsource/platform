7186e68ac7b36fd3a513c5cc70761edf6beb7388
Anita Amini <baadge29@yahoo.co.uk>
2015-03-30 22:42:17 +0100

    some more test, controllers and the jade upload page

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,7 +1,7 @@
 var controller = require('../controllers/controller.js');
 
 module.exports = [
-	{path: "/{file*}",					method: "GET",	config: controller.serveFile},
+	{path: "/{file*}",					method: "GET",		config: controller.serveFile},
 
 	{path: '/', 						method: 'GET', 		config: controller.homeView},
 	{path: '/login', 					method: 'POST', 	config: controller.login},
