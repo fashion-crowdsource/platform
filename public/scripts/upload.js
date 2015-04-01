88eaf93b6b911e2fffa606a322a217255c338166
amilvasishtha <amil_v@msn.com>
2015-04-01 13:32:09 +0100

    working on delete button

--- public/scripts/upload.js
+++ public/scripts/upload.js
@@ -5,24 +5,29 @@ function loadImg(input) {
 	  reader.onload = function(e){
 		$('#preview')
 		  .attr('src', e.target.result)
-		  .width(170)
-		  .height(170);
+		  .width(200)
+		  .height(200);
 	  };
 		reader.readAsDataURL(input.files[0]);
 	}
 }
 
 
- $('a.delete').on('click',function(e){
-        e.preventDefault();
-        imageID = $(this).closest('input')[0].id;
-        // alert('Now deleting "'+imageID+'"');
-        $(this).closest('input')
-            // .fadeTo(300,0,function(){
-            //     $(this)
-            //         .animate({width:0},200,function(){
-            //             $(this)
-                            .remove();
-                    });
-            // });
-    // });
+ // $('a.delete').on('click',function(e){
+ //        e.preventDefault();
+ //        imageID = $(this).closest('input')[0].id;
+ //        // alert('Now deleting "'+imageID+'"');
+ //        $(this).closest('input')
+ //            // .fadeTo(300,0,function(){
+ //            //     $(this)
+ //            //         .animate({width:0},200,function(){
+ //            //             $(this)
+ //                            .remove();
+ //                    });
+ //            // });
+ //    // });
+
+
+$("bin").click(function() {
+  $("#").remove();
+});
