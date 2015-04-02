a8ec329b32cedc3bafcdc8db4df0481e207510ba
Adam Kowalczyk <adamkowalczyk@gmail.com>
2015-04-02 10:00:45 +0100

    add images/files to user docs

--- api/models/schemae.js
+++ api/models/schemae.js
@@ -24,7 +24,34 @@ designSchema.plugin(crate, {
 		region: config.region,
 	}),
 	fields: {
-		image: {
+		designMainImage: {
+			processor: new ImageMagick({
+				// tmpDir: '/tmp', // Where transformed files are placed before storage, defaults to os.tmpdir()
+				// formats: ['JPEG', 'GIF', 'PNG'], // Supported formats, defaults to ['JPEG', 'GIF', 'PNG', 'TIFF']
+				transforms: {
+					original: {
+						// keep the original file
+					},
+					small: {
+						resize: '200x200',
+						format: '.jpg'
+					},
+					medium: {
+						resize: '300x300',
+						format: '.jpg'
+					},
+					large: {
+						resize: '500x500',
+						format: '.jpg'
+					},
+					extraLarge: {
+						resize: '1000x1000',
+						format: '.jpg'
+					}
+				}
+			})
+		},
+		designAdditionalImages: {
 			array: true,
 			processor: new ImageMagick({
 				// tmpDir: '/tmp', // Where transformed files are placed before storage, defaults to os.tmpdir()
@@ -61,12 +88,53 @@ var userSchema = new Schema({
 	email: 			{type: String, required: true, unique: true},
 	firstName: 		{type: String, required: true},
 	surname: 		{type: String, required: true},
-	phoneNumber: 	{type: String, required: true},
+	phoneNumber: 	{type: String}, //optional --NB. must make sure phone/address kept private, admin use only
 	address: 		{type: String}, //optional
 	bio: 			{type: String}, //optional
+	links: 			[String], 		//optional
 	dateJoined: 	{type: Date, required: true},
 	isAdmin: 		{type: Boolean, required: true, default: false},
+	// isDesigner	{type: Boolean, required: true, default: false}, //if taking user signups, e.g. to preorder
 	designIds: 		[ObjectId]
+});
+
+
+userSchema.plugin(crate, {
+	storage: new S3({
+		key: config.key,
+		secret: config.secret,
+		bucket: config.bucket,
+		acl: config.acl,
+		region: config.region,
+	}),
+	fields: {
+		profileImage: {
+			processor: new ImageMagick({
+				// tmpDir: '/tmp', // Where transformed files are placed before storage, defaults to os.tmpdir()
+				// formats: ['JPEG', 'GIF', 'PNG'], // Supported formats, defaults to ['JPEG', 'GIF', 'PNG', 'TIFF']
+				transforms: {
+					original: {
+						// keep the original file
+					},
+					small: {
+						resize: '200x200',
+						format: '.jpg'
+					},
+					medium: {
+						resize: '300x300',
+						format: '.jpg'
+					},
+					large: {
+						resize: '500x500',
+						format: '.jpg'
+					}
+				}
+			})
+		},
+		additionalFiles: {
+			array: true
+		}
+	}
 });
 
 var User 	= mongoose.model('user', userSchema, 'users');
