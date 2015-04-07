76cf2e0818b94579553f2179512057bb70f504a7
Anita Amini <baadge29@yahoo.co.uk>
2015-04-07 12:44:28 +0100

    admin stuff

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,13 +1,6 @@
 // Admin page
 //$(document).ready(function(){
-
-$( ".designThumbnail" ).hover(
-	function() {
-	  $( this ).toggleClass( $("active")
-		.next()
-          .stop( true, true );
-});
-//doesnt work at the mo but will make it work tomorrow
-
-
-
+//   $(".designBox").hover(function() {
+//      $(this).find(".hoverName").toggle("display:inline");
+//   });
+//});
